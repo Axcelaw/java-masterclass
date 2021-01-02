c5c0c4343da56e2580f5c2a8145d67fa5c479c45
Walace <walace.andr.m@gmail.com>
2021-01-02 14:54:29 -0300

    Coding Exercise 37: Composition

---
+++ src/com/walace/ex37/Wall.java
@@ -0,0 +1,14 @@
+package com.walace.ex37;
+
+public class Wall {
+
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
